a86429dbca3c30b65ef033abbd50ca5b79e2824b
aakanshaparmar <parmar.aakansha@gmail.com>
2016-03-26 14:41:00 +0800

    Small Changes

--- app/src/main/java/aakanshaparmar/blackboxsystem/SlidingTabFragment3.java
+++ app/src/main/java/aakanshaparmar/blackboxsystem/SlidingTabFragment3.java
@@ -5,11 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 public class SlidingTabFragment3 extends Fragment  {
 
-   public static final String ARG_OBJECT = "object";
+    ImageButton sosButton;
+    PopupWindow popUp;
+    FrameLayout mainLayout;
 
+    public static final String ARG_OBJECT = "object";
 
     @Override
     public View onCreateView(final LayoutInflater inflater,
@@ -17,9 +24,26 @@ public class SlidingTabFragment3 extends Fragment  {
         View rootView;
         rootView = inflater.inflate(R.layout.fragment_sliding_tab_fragment3, container, false);
 
+        sosButton = (ImageButton) rootView.findViewById(R.id.imageButton);
+        popUp = new PopupWindow(getActivity());
+        mainLayout = new FrameLayout(rootView.getContext());
+
+        sosButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "You have pressed SOS! We'll get you help! Please Wait", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
         return rootView;
     }
 
 
 
 }
+
+
+
