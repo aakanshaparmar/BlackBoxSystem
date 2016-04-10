79c2f590b3570e54b91796833df9eba2ca4fd1f7
aakanshaparmar <parmar.aakansha@gmail.com>
2016-04-11 02:47:14 +0800

    Address Card Built and Implemented

--- app/src/main/java/aakanshaparmar/blackboxsystem/ElderlyAddressCard.java
+++ app/src/main/java/aakanshaparmar/blackboxsystem/ElderlyAddressCard.java
@@ -1,13 +1,29 @@
 package aakanshaparmar.blackboxsystem;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ElderlyAddressCard extends AppCompatActivity {
+
+    TextView addressField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elderly_address_card);
+
+        addressField = (TextView) findViewById(R.id.address);
+
+        //Retrieve Address
+        SharedPreferences sharedPreferences = getSharedPreferences("aakanshaparmar.blackboxsystem", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String address = sharedPreferences.getString("address", "");
+
+        addressField.setText(address);
+
     }
 }
