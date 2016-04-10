64562ceb5fe4954695c8e9b0b59633a78b6b417a
aakanshaparmar <parmar.aakansha@gmail.com>
2016-04-11 00:56:25 +0800

    Directions to Home Origin and destination extraction made

--- app/src/main/java/aakanshaparmar/blackboxsystem/ElderlyShowPathToHome.java
+++ app/src/main/java/aakanshaparmar/blackboxsystem/ElderlyShowPathToHome.java
@@ -1,12 +1,21 @@
 package aakanshaparmar.blackboxsystem;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -33,7 +42,7 @@ import java.util.List;
 public class ElderlyShowPathToHome extends FragmentActivity {
 
     //private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543, -73.998585);
-    private static final LatLng WALL_STREET = new LatLng(40.7064, -74.0094);
+   // private static final LatLng WALL_STREET = new LatLng(22.2844, 114.1345);
 
     LatLng origin;
     LatLng destination;
@@ -41,20 +50,30 @@ public class ElderlyShowPathToHome extends FragmentActivity {
     GoogleMap map;
     ArrayList<LatLng> markerPoints;
 
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elderly_show_path_to_home);
 
-        //Retrieve origin i.e. home
+        //Retrieve destination i.e. home from Shared Preferences
+        SharedPreferences sharedPreferences = getSharedPreferences("aakanshaparmar.blackboxsystem", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        double latitude = Double.longBitsToDouble(sharedPreferences.getLong("homeLat", 0));
+        double longitude = Double.longBitsToDouble(sharedPreferences.getLong("homeLng", 0));
 
+        //Save it in destination
+        destination = new LatLng(latitude, longitude);
 
+        //Get Current location to be the origin
+        getCurrentLocation();
 
         // Initializing
         markerPoints = new ArrayList<LatLng>();
 
         // Getting reference to SupportMapFragment of the activity_main
-        SupportMapFragment fm = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         // Getting Map for the SupportMapFragment
         map = fm.getMap();
@@ -65,65 +84,93 @@ public class ElderlyShowPathToHome extends FragmentActivity {
         markerPoints.add(origin);
         MarkerOptions options = new MarkerOptions();
         options.position(origin);
-        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
         map.addMarker(options);
 
         //CREATING MARKER FOR SECOND LOCATION
-        markerPoints.add(WALL_STREET);
+        markerPoints.add(destination);
         MarkerOptions options1 = new MarkerOptions();
-        options1.position(WALL_STREET);
-        options1.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        options1.position(destination);
+        options1.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
         map.addMarker(options1);
 
         //Focus Map to origin location
         CameraPosition camPos = new CameraPosition.Builder()
                 .target(origin)
-                .zoom(15)
+                .zoom(18)
                 .build();
         CameraUpdate camUpd3 = CameraUpdateFactory.newCameraPosition(camPos);
         map.animateCamera(camUpd3);
 
 
         //Retrieve URL for directions
-        String DirectionRequestUrl = getDirectionsUrl(origin, WALL_STREET);
+        String DirectionRequestUrl = getDirectionsUrl(origin, destination);
         DownloadTask downloadTask = new DownloadTask();
 
         // Start downloading json data from Google Directions API
         downloadTask.execute(DirectionRequestUrl);
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private void getCurrentLocation()
+    {
+        // Getting LocationManager object from System Service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // Creating a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Getting the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Getting Current Location
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        //Converting location to separate latitude and longitude values
+        double latitude = location.getLatitude();
+
+        // Getting longitude of the current location
+        double longitude = location.getLongitude();
+
+        // Creating a LatLng object for the current location
+        origin= new LatLng(latitude, longitude);
+
     }
 
     //Method to get url info
-    private String getDirectionsUrl(LatLng origin,LatLng dest){
+    private String getDirectionsUrl(LatLng origin, LatLng dest) {
 
         // Origin of route
-        String str_origin = "origin="+origin.latitude+","+origin.longitude;
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
 
         // Destination of route
-        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
 
         // Sensor enabled
         String sensor = "sensor=false";
 
         // Building the parameters to the web service
-        String parameters = str_origin+"&"+str_dest+"&"+sensor+"&mode=walking";
+        String parameters = str_origin + "&" + str_dest + "&" + sensor + "&mode=walking";
 
         // Output format
         String output = "json";
 
         // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
 
         return url;
     }
 
-   //A method to download json data from url
+    //A method to download json data from url
     private String downloadUrl(String strUrl) throws IOException {
         String data = "";
         InputStream iStream = null;
         HttpURLConnection urlConnection = null;
-        try{
+        try {
             URL url = new URL(strUrl);
 
             // Creating an http connection to communicate with url
@@ -140,7 +187,7 @@ public class ElderlyShowPathToHome extends FragmentActivity {
             StringBuffer sb = new StringBuffer();
 
             String line = "";
-            while( ( line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 sb.append(line);
             }
 
@@ -148,13 +195,53 @@ public class ElderlyShowPathToHome extends FragmentActivity {
 
             br.close();
 
-        }catch(Exception e){
+        } catch (Exception e) {
             Log.d("Error while downloading url", e.toString());
-        }finally{
+        } finally {
             iStream.close();
             urlConnection.disconnect();
         }
         return data;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "ElderlyShowPathToHome Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://aakanshaparmar.blackboxsystem/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "ElderlyShowPathToHome Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://aakanshaparmar.blackboxsystem/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 
     // Fetches data from url passed
@@ -167,11 +254,11 @@ public class ElderlyShowPathToHome extends FragmentActivity {
             // For storing data from web service
             String data = "";
 
-            try{
+            try {
                 // Fetching the data from web service
                 data = downloadUrl(url[0]);
-            }catch(Exception e){
-                Log.d("Background Task",e.toString());
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
             }
             return data;
         }
@@ -190,7 +277,7 @@ public class ElderlyShowPathToHome extends FragmentActivity {
     }
 
     /// A class to parse the Google Places in JSON format
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
 
         // Parsing the data in non-ui thread
         @Override
@@ -199,13 +286,13 @@ public class ElderlyShowPathToHome extends FragmentActivity {
             JSONObject jObject;
             List<List<HashMap<String, String>>> routes = null;
 
-            try{
+            try {
                 jObject = new JSONObject(jsonData[0]);
                 PathJSONParser parser = new PathJSONParser();
 
                 // Starts parsing data
                 routes = parser.parse(jObject);
-            }catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return routes;
@@ -219,7 +306,7 @@ public class ElderlyShowPathToHome extends FragmentActivity {
             MarkerOptions markerOptions = new MarkerOptions();
 
             // Traversing through all the routes
-            for(int i=0;i<result.size();i++){
+            for (int i = 0; i < result.size(); i++) {
                 points = new ArrayList<LatLng>();
                 lineOptions = new PolylineOptions();
 
@@ -227,8 +314,8 @@ public class ElderlyShowPathToHome extends FragmentActivity {
                 List<HashMap<String, String>> path = result.get(i);
 
                 // Fetching all the points in i-th route
-                for(int j=0;j<path.size();j++){
-                    HashMap<String,String> point = path.get(j);
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
 
                     double lat = Double.parseDouble(point.get("lat"));
                     double lng = Double.parseDouble(point.get("lng"));
